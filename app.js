438e4cb52bd058d4e0311bd577c58005e5604468
emxis <106862004+emxis@users.noreply.github.com>
2023-06-28 15:21:42 +0300

    nodejs-mp3-downloader

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/page/index.html");
+});
+
+app.post("/download", async (req, res) => {
+    const url = req.body.url;
+    const output = __dirname + '/audio.mp3';
+    const music = ytdl(url, { quality: "highestaudio" });
+
+    music.pipe(fs.createWriteStream(output));
+
+    music.on('end', () => {
+        res.download(output, 'audio.mp3', () => {
+            fs.unlinkSync(output);
+        });
+    });
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Port ${port} üzerinde çalışıyor.`));
